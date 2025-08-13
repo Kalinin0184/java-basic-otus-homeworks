bbb2144b1263ff119137ccb5dbf5aa6e1fb1554f
Daniil <danya.kalinin.93@mail.ru>
2025-08-13 18:18:07 +0300

    Выполнен метод selectColor

--- src/main/java/ru/otus/java/basic/homeworks/homework3/Homework3.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework3/Homework3.java
@@ -1,8 +1,12 @@
 package ru.otus.java.basic.homeworks.homework3;
 
+import java.util.Random;
+
 public class Homework3 {
     public static void main(String[] args) {
         greetings();
+        checkSign(3, 4, 5);
+        selectColor();
     }
 
     public static void greetings() {
@@ -14,13 +18,21 @@ public class Homework3 {
 
     public static void checkSign(int a, int b, int c) {
         int sum = a + b + c;
-        if (sum >= 0)
-        {
+        if (sum >= 0) {
             System.out.println("Сумма положительная");
-        }
-        else
-        {
+        } else {
             System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void selectColor() {
+        int data = (int) (Math.random() * (30 - 0 + 1));
+        if (data <= 10) {
+            System.out.println("Красный");
+        } else if (data <= 20) {
+            System.out.println("Желтый");
+        } else {
+            System.out.println("Зеленый");
         }
     }
 }
