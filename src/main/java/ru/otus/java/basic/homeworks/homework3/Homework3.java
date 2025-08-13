57d2087299937341324d8628582fe2481c8addaf
Daniil <danya.kalinin.93@mail.ru>
2025-08-13 19:21:37 +0300

    Домашнее задание выполнено

--- src/main/java/ru/otus/java/basic/homeworks/homework3/Homework3.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework3/Homework3.java
@@ -1,14 +1,42 @@
 package ru.otus.java.basic.homeworks.homework3;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Homework3 {
     public static void main(String[] args) {
-        greetings();
-        checkSign(3, 4, 5);
-        selectColor();
-        compareNumbers();
-        addOrSubtractAndPrint(10, 5, false);
+        Scanner scanner = new Scanner(System.in);
+        int dataEntry, a, b, c, initValue, delta;
+        boolean increment;
+        System.out.println("Введите число от 1 до 5:");
+        dataEntry = scanner.nextInt();
+        if (dataEntry >= 1 && dataEntry <= 5) {
+            switch (dataEntry) {
+                case 1:
+                    greetings();
+                    break;
+                case 2:
+                    a = (int) (Math.random() * 40 - 20);
+                    b = (int) (Math.random() * 40 - 20);
+                    c = (int) (Math.random() * 40 - 20);
+                    checkSign(a, b, c);
+                    break;
+                case 3:
+                    selectColor();
+                    break;
+                case 4:
+                    compareNumbers();
+                    break;
+                case 5:
+                    initValue = (int) (Math.random() * 50 + 1);
+                    delta = (int) (Math.random() * 50 + 1);
+                    increment = new Random().nextBoolean();
+                    addOrSubtractAndPrint(initValue, delta, increment);
+                    break;
+            }
+        } else {
+            System.out.println("Error! Введите число из заданного диапозона");
+        }
     }
 
     public static void greetings() {
@@ -28,7 +56,7 @@ public class Homework3 {
     }
 
     public static void selectColor() {
-        int data = (int) (Math.random() * (30 - 0 + 1));
+        int data = (int) (Math.random() * 30 + 1);
         if (data <= 10) {
             System.out.println("Красный");
         } else if (data <= 20) {
@@ -39,8 +67,8 @@ public class Homework3 {
     }
 
     public static void compareNumbers() {
-        int a = (int) (Math.random() * (100 - 0 + 1));
-        int b = (int) (Math.random() * (100 - 0 + 1));
+        int a = (int) (Math.random() * (100 + 1));
+        int b = (int) (Math.random() * (100 + 1));
         if (a >= b) {
             System.out.println("a >= b");
         } else {
@@ -49,7 +77,7 @@ public class Homework3 {
     }
 
     public static void addOrSubtractAndPrint(int initValue, int delta, boolean increment) {
-        if (increment == true) {
+        if (increment) {
             initValue += delta;
             System.out.println(initValue);
         } else {
