cf123de54017a628d4b747b06fd9de48bfce80ef
Daniil <danya.kalinin.93@mail.ru>
2025-08-13 04:08:51 +0300

    Выполнен метод greetings

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework3/Homework3.java
@@ -0,0 +1,14 @@
+package ru.otus.java.basic.homeworks.homework3;
+
+public class Homework3 {
+    public static void main(String[] args) {
+        greetings();
+    }
+
+    public static void greetings() {
+        System.out.println("Hello");
+        System.out.println("World");
+        System.out.println("from");
+        System.out.println("Java");
+    }
+}
