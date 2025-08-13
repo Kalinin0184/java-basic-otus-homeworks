3db593d0ef9a6ca447765632b955b239d872875a
Daniil <danya.kalinin.93@mail.ru>
2025-08-13 04:12:06 +0300

    Выполнен метод checkSign

--- src/main/java/ru/otus/java/basic/homeworks/homework3/Homework3.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework3/Homework3.java
@@ -11,4 +11,16 @@ public class Homework3 {
         System.out.println("from");
         System.out.println("Java");
     }
+
+    public static void checkSign(int a, int b, int c) {
+        int sum = a + b + c;
+        if (sum >= 0)
+        {
+            System.out.println("Сумма положительная");
+        }
+        else
+        {
+            System.out.println("Сумма отрицательная");
+        }
+    }
 }
