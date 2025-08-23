e9653911251efaa9e8e5f69fef692e635935f927
Daniil <danya.kalinin.93@mail.ru>
2025-08-23 16:20:04 +0300

    Реализован метод printLine

--- src/main/java/ru/otus/java/basic/homeworks/Application.java
+++ src/main/java/ru/otus/java/basic/homeworks/Application.java
@@ -4,4 +4,11 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("Hello world");
     }
+
+    public static void printLine(int numberPrinting, String strPrint)
+    {
+        for (int i = 0; i < numberPrinting; i++) {
+            System.out.println(strPrint);
+        }
+    }
 }
