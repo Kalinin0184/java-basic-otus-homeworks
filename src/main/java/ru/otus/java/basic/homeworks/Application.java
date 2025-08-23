6c1af32586bb5914450ddd2f325e295a5db00f7f
Daniil <danya.kalinin.93@mail.ru>
2025-08-23 16:31:06 +0300

    Реализован метод sumArr

--- src/main/java/ru/otus/java/basic/homeworks/Application.java
+++ src/main/java/ru/otus/java/basic/homeworks/Application.java
@@ -3,6 +3,8 @@ package ru.otus.java.basic.homeworks;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        printLine(6, "Привет мир!");
+        sumArr(3,4,5,6,7,8,9);
     }
 
     public static void printLine(int numberPrinting, String strPrint)
@@ -10,5 +12,17 @@ public class Application {
         for (int i = 0; i < numberPrinting; i++) {
             System.out.println(strPrint);
         }
+    }
+
+    public static void sumArr(int... array)
+    {
+        int sumA = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > 5)
+            {
+                sumA += array[i];
+            }
+        }
+        System.out.println(sumA);
     }
 }
