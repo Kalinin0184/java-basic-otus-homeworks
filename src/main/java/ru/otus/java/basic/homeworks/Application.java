ebdbb99da97acc23b3db6cab8fe483f815bb569e
Daniil <danya.kalinin.93@mail.ru>
2025-08-23 16:42:38 +0300

    Реализован метод fillArr

--- src/main/java/ru/otus/java/basic/homeworks/Application.java
+++ src/main/java/ru/otus/java/basic/homeworks/Application.java
@@ -1,10 +1,14 @@
 package ru.otus.java.basic.homeworks;
 
+import java.util.Arrays;
+
 public class Application {
     public static void main(String[] args) {
         System.out.println("Hello world");
         printLine(6, "Привет мир!");
-        sumArr(3,4,5,6,7,8,9);
+        sumArr(new int[] {3,4,5,6,7,8,9});
+        int[] arrayFill = new int[10];
+        fillArr(4, arrayFill);
     }
 
     public static void printLine(int numberPrinting, String strPrint)
@@ -14,7 +18,7 @@ public class Application {
         }
     }
 
-    public static void sumArr(int... array)
+    public static void sumArr(int[] array)
     {
         int sumA = 0;
         for (int i = 0; i < array.length; i++) {
@@ -24,5 +28,11 @@ public class Application {
             }
         }
         System.out.println(sumA);
+    }
+
+    public static void fillArr(int variableNumber, int[] arrayFill)
+    {
+        Arrays.fill(arrayFill, variableNumber);
+        System.out.println(Arrays.toString(arrayFill));
     }
 }
