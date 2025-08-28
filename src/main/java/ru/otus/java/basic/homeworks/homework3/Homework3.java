93121e5702f7d4d06d390cc9ad79ed5ab597aa5d
Daniil <danya.kalinin.93@mail.ru>
2025-08-28 19:48:54 +0300

    Реализован метод drawSquare

--- src/main/java/ru/otus/java/basic/homeworks/homework3/Homework3.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework3/Homework3.java
@@ -4,6 +4,7 @@ public class Homework3 {
     public static void main(String[] args) {
         int[][] array = {{-1,-2,-3}, {1,1,1}, {3,3,3}};
         sumOfPositiveElements(array);
+        drawSquare(6);
     }
 
     public static void sumOfPositiveElements (int[][] array)
@@ -19,4 +20,12 @@ public class Homework3 {
         System.out.println(sum);
     }
 
+    public static void drawSquare(int size) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
 }
