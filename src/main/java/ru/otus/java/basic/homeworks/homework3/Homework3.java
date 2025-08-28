95c10364399c65212837775cd8fe19e85146e4d4
Daniil <danya.kalinin.93@mail.ru>
2025-08-28 19:58:01 +0300

    Реализован метод zeroingArray

--- src/main/java/ru/otus/java/basic/homeworks/homework3/Homework3.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework3/Homework3.java
@@ -1,10 +1,14 @@
 package ru.otus.java.basic.homeworks.homework3;
 
+import java.util.Arrays;
+
 public class Homework3 {
     public static void main(String[] args) {
         int[][] array = {{-1,-2,-3}, {1,1,1}, {3,3,3}};
         sumOfPositiveElements(array);
         drawSquare(6);
+        int[][] arrayZero = {{3,4,5}, {1,6,7}, {4,4,4}};
+        zeroingArray(arrayZero);
     }
 
     public static void sumOfPositiveElements (int[][] array)
@@ -24,6 +28,16 @@ public class Homework3 {
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void zeroingArray(int[][] arrayZero){
+        for (int i = 0; i < arrayZero.length; i++) {
+            arrayZero[i][i] = 0;
+            for (int j = 0; j < arrayZero.length; j++) {
+                System.out.print(" " + arrayZero[i][j] + " ");
             }
             System.out.println();
         }
