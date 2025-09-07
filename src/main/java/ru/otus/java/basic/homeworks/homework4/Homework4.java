a394466138ec68b71787ecd043912ab888057ac3
Daniil <danya.kalinin.93@mail.ru>
2025-09-07 11:05:45 +0300

    Реализован класс User

---
+++ src/main/java/ru/otus/java/basic/homeworks/homework4/Homework4.java
@@ -0,0 +1,7 @@
+package ru.otus.java.basic.homeworks.homework4;
+
+public class Homework4 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/main/java/ru/otus/java/basic/homeworks/homework4/User.java
@@ -0,0 +1,9 @@
+package ru.otus.java.basic.homeworks.homework4;
+
+public class User {
+    private String name;
+    private String surname;
+    private String patronymic;
+    private String birthday;
+    private String email;
+}
