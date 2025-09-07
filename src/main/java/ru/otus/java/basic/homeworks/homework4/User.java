9d2d4a88448e9b82c0d5be8f443e3fed077a1e15
Daniil <danya.kalinin.93@mail.ru>
2025-09-07 15:06:23 +0300

    Реализован массив из 10 пользователей и цикл вывода возрастом больше 40

--- src/main/java/ru/otus/java/basic/homeworks/homework4/User.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework4/User.java
@@ -4,10 +4,10 @@ public class User {
     private String name;
     private String surname;
     private String patronymic;
-    private String birthday;
+    private int birthday;
     private String email;
 
-    public User(String name, String surname, String patronymic, String birthday, String email) {
+    public User(String surname, String name, String patronymic, int birthday, String email) {
         this.name = name;
         this.surname = surname;
         this.patronymic = patronymic;
@@ -15,9 +15,49 @@ public class User {
         this.email = email;
     }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPatronymic() {
+        return patronymic;
+    }
+
+    public void setPatronymic(String patronymic) {
+        this.patronymic = patronymic;
+    }
+
+    public int getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(int birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     public void info() {
         System.out.println("ФИО: " + surname + " " + name + " " + patronymic);
         System.out.println("Год рождения: " + birthday);
-        System.out.println("email: " + email);
+        System.out.println("e-mail: " + email);
     }
 }
