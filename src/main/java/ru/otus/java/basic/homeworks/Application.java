036be52f7cb1c05201457cb2c0ca3951fc3ac2d3
Daniil <danya.kalinin.93@mail.ru>
2025-10-02 00:10:37 +0300

    Реализовано дз

--- src/main/java/ru/otus/java/basic/homeworks/Application.java
+++ src/main/java/ru/otus/java/basic/homeworks/Application.java
@@ -4,19 +4,5 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Application {
-    public static void main(String[] args) {
-        Cat[] cats =
-                {
-                        new Cat("Barsik", 7),
-                        new Cat("Murka", 11),
-                        new Cat("Malish", 14)};
-        ArrayList<Cat> arrayCats = new ArrayList<>(Arrays.asList(cats));
-        Plates plate = new Plates(32);
-        for (int i = 0; i < arrayCats.size(); i++) {
-            arrayCats.get(i).eatFood(plate);
-        }
-        for (int i = 0; i < arrayCats.size(); i++) {
-            arrayCats.get(i).info();
-        }
-    }
+
 }
--- src/main/java/ru/otus/java/basic/homeworks/Homework6.java
+++ src/main/java/ru/otus/java/basic/homeworks/Homework6.java
@@ -1,8 +1,22 @@
 package ru.otus.java.basic.homeworks;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Homework6 {
     public static void main(String[] args) {
-        Plates plate = new Plates(10);
-
+        Cat[] cats =
+                {
+                        new Cat("Barsik", 7),
+                        new Cat("Murka", 11),
+                        new Cat("Malish", 14)};
+        ArrayList<Cat> arrayCats = new ArrayList<>(Arrays.asList(cats));
+        Plates plate = new Plates(32);
+        for (int i = 0; i < arrayCats.size(); i++) {
+            arrayCats.get(i).eatFood(plate);
+        }
+        for (int i = 0; i < arrayCats.size(); i++) {
+            arrayCats.get(i).info();
+        }
     }
 }
