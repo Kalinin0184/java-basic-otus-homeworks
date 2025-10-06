6f3e8edcbad93a1f0d7fcdf1cf55c6f32580a89b
Daniil <danya.kalinin.93@mail.ru>
2025-10-06 14:42:53 +0300

    Реализовано дз

--- src/main/java/ru/otus/java/basic/homeworks/Homework6.java
+++ src/main/java/ru/otus/java/basic/homeworks/Homework6.java
@@ -5,12 +5,9 @@ import java.util.Arrays;
 
 public class Homework6 {
     public static void main(String[] args) {
-        Cat[] cats =
-                {
-                        new Cat("Barsik", 7),
-                        new Cat("Murka", 11),
-                        new Cat("Malish", 14)};
-        ArrayList<Cat> arrayCats = new ArrayList<>(Arrays.asList(cats));
+        ArrayList<Cat> arrayCats = new ArrayList<>(Arrays.asList(new Cat("Barsik", 7),
+                new Cat("Murka", 11),
+                new Cat("Malish", 14)));
         Plates plate = new Plates(32);
         for (int i = 0; i < arrayCats.size(); i++) {
             arrayCats.get(i).eatFood(plate);
