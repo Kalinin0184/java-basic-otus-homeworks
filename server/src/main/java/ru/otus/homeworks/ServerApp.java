72eea98e998b9d05ed7bfa0559d82414a30f2f64
Daniil <danya.kalinin.93@mail.ru>
2025-11-22 22:30:27 +0300

    Реализовано дз

---
+++ server/src/main/java/ru/otus/homeworks/ServerApp.java
@@ -0,0 +1,9 @@
+package ru.otus.homeworks;
+
+public class ServerApp {
+    public static final int PORT = 8189;
+
+    public static void main(String[] args) {
+        new Server(PORT).start();
+    }
+}
